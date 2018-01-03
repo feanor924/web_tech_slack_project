3ecbb8120689bdeafb52ee5b6033b55ff0bc2edd
Caner Bakar <canerbakar@Caners-MacBook-Pro.local>
2018-01-03 20:24:00 +0300

    search.messages

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
 import os
 from slackclient import SlackClient
 
-
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN', None)
 
 slack_client = SlackClient(SLACK_TOKEN)
 
 
-
-def list_channels():
-    channels_call = slack_client.api_call("channels.list")
+def list_messages(message):
+    channels_call = slack_client.api_call("search.messages", query = message)
     if channels_call['ok']:
-        return channels_call['channels']
-    return None
-
-
-def channel_info(channel_id):
-    channel_info = slack_client.api_call("channels.info", channel=channel_id)
-    if channel_info:
-        return channel_info['channel']
+        return channels_call['messages']
     return None
 
 
 if __name__ == '__main__':
-    channels = list_channels()
-    if channels:
-        print("Channels: ")
-        for c in channels:
-            print(c['name'] + " (" + c['id'] + ")")
-            detailed_info = channel_info(c['id'])
-            if detailed_info:
-                print(detailed_info['latest']['text'])
+    
+    message = raw_input('Enter message you want to search: ')
+    messages = list_messages(message)
+    
+    if messages:
+        matches = messages['matches']
+        if matches: 
+            print("Messages: ")
+            for m in matches:
+                channel = m['channel'];
+                if ( channel['is_channel']  == True ):
+                    print("User: " + m['username'] + " / Message: " + m['text'] + " / Channel Name: " + channel['name'])
+                else:
+                    print("User: " + m['username'] + " / Message: " + m['text'] + " / Not in channel ")
+        else:
+            print("None matches.")
+                
     else:
         print("Unable to authenticate.")
